ca7fdf624ca4d0f46c27ba961870591a6aae35d6
Kshama Tomer <114792238+Kshama1234@users.noreply.github.com>
2024-07-16 11:49:36 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char ch = '1';
+    int num = 1;
+    cout<<endl;
+    switch ( ch ) {
+        case 1: cout<<"First"<<endl;
+        break;
+
+        case '1': switch(num) {
+           case 1: cout<<"value of num is: "<<num<<endl;
+            break;
+        }
+        break;
+
+        default: cout<<"It is a default case"<<endl;
+        break;
+    }
+    cout<<endl;
+    return 0;
+}
